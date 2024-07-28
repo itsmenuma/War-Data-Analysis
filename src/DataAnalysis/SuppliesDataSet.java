8f0a58e9dcd30884ff6847f5c2380b8b478f6443
itsmenuma <numarahamath@gmail.com>
2024-07-28 22:23:31 +0530

    Initial Commit

---
+++ src/DataAnalysis/SuppliesDataSet.java
@@ -0,0 +1,32 @@
+package DataAnalysis;
+import java.sql.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SuppliesDataSet {
+    private static final String url = "jdbc:mysql://localhost:3306/war";
+    private static final String user = "root";
+    private static final String password = "rayees@123";
+
+    public static Map<String, Integer> getPersonnelStatusCount() {
+        Map<String, Integer> personnelStatusCount = new HashMap<>();
+
+        String query = "SELECT status, COUNT(*) AS count FROM Supplies GROUP BY status";
+
+        try (Connection conn = DriverManager.getConnection(url, user, password);
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(query)) {
+
+            while (rs.next()) {
+                String status = rs.getString("status");
+                int count = rs.getInt("count");
+                personnelStatusCount.put(status, count);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return personnelStatusCount;
+    }
+}
---
+++ src/module-info.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module WarAnalysis {
+	requires java.desktop;
+	requires java.sql;
+	
+}
