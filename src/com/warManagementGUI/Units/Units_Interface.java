2bb24d82882e8522416e646b05c6dccb242fcaef
Numa Rahamath S A <134629068+itsmenuma@users.noreply.github.com>
2025-05-14 20:06:35 +0530

    Update Units_Interface.java

--- src/com/warManagementGUI/Units/Units_Interface.java
+++ src/com/warManagementGUI/Units/Units_Interface.java
@@ -194,7 +194,7 @@ public class Units_Interface extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 				String url = "jdbc:mysql://localhost:3306/war";
 		        String user = "root";
-		        String password = "rayees@123";
+		        String password = "PASSWORD";
 
 		        String insertQuery = "INSERT INTO units (unit_id, unit_name, unit_type, commander_id, location_id) VALUES (?, ?, ?, ?, ?)";
 		        try (Connection conn = DriverManager.getConnection(url, user, password);
@@ -237,7 +237,7 @@ public class Units_Interface extends JFrame {
 
 			    String url = "jdbc:mysql://localhost:3306/war";
 			    String user = "root";
-			    String password = "rayees@123";
+			    String password = "PASSWORD";
 
 			    String selectQuery = "SELECT * FROM units";
 
@@ -274,7 +274,7 @@ public class Units_Interface extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 				 String url = "jdbc:mysql://localhost:3306/war";
 			        String user = "root";
-			        String password = "rayees@123";
+			        String password = "PASSWORD";
 
 			        String updateQuery = "UPDATE units SET unit_name = ?, unit_type = ?, commander_id = ?, location_id = ? WHERE unit_id = ?";
 			        
@@ -318,7 +318,7 @@ public class Units_Interface extends JFrame {
 
 				    String url = "jdbc:mysql://localhost:3306/war";
 				    String user = "root";
-				    String password = "rayees@123";
+				    String password = "PASSWORD";
 
 				    String selectQuery = "SELECT * FROM units";
 
@@ -357,7 +357,7 @@ public class Units_Interface extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 				String url = "jdbc:mysql://localhost:3306/war";
 		        String user = "root";
-		        String password = "rayees@123";
+		        String password = "PASSWORD";
 
 		        String deleteQuery = "DELETE FROM units WHERE unit_id = ?";
 		        
@@ -402,7 +402,7 @@ public class Units_Interface extends JFrame {
 
 	    String url = "jdbc:mysql://localhost:3306/war";
 	    String user = "root";
-	    String password = "rayees@123";
+	    String password = "PASSWORD";
 
 	    String selectQuery = "SELECT * FROM units";
 
