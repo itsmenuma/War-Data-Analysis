d01c37468023d4ddd47635d0923d5844972545a2
JaiSwarup <jai.swarup.ug22@nsut.ac.in>
2025-05-28 00:10:52 +0530

    feat:migrate charts to gui

--- src/com/warManagementGUI/DataAnalysis/MissionsBarChart.java
+++ src/com/warManagementGUI/DataAnalysis/MissionsBarChart.java
@@ -2,23 +2,22 @@ package com.warManagementGUI.DataAnalysis;
 
 import java.util.Map;
 
-import javax.swing.JFrame;
-
 import com.warManagementGUI.util.DBUtil;
 
+import javafx.stage.Stage;
+
 public class MissionsBarChart extends AbstractBarChart {
-    private static final long serialVersionUID = 1L;
 
     public MissionsBarChart(Map<String, Integer> data) {
         super(data, "Missions Analysis");
     }
     
-    public static JFrame showMissionStatusChart() {
+    public static Stage showMissionStatusChart() {
         Map<String, Integer> statusCount = DBUtil.getStatusCount("missions");
         return showBarChart("Missions by Status", statusCount);
     }
     
-    public static JFrame showMissionTypeChart() {
+    public static Stage showMissionTypeChart() {
         Map<String, Integer> typeCount = DBUtil.getGroupCount("missions", "mission_type");
         return showBarChart("Missions by Type", typeCount);
     }
