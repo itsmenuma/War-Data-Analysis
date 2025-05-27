b6ce14a6e61ff509bb103224d10204e6db509844
SATYAM GUPTA <143061930+Satyamgupta2365@users.noreply.github.com>
2025-05-27 17:08:58 +0530

    module-info.java

--- src/module-info.java
+++ src/module-info.java
@@ -9,3 +9,9 @@ module WarAnalysis {
 	requires java.sql;
 	
 }
+module WarDataAnalysis {
+    requires opencsv;
+    requires org.jfree.chart;
+    requires org.geotools;
+    requires edu.stanford.nlp.core;
+}
